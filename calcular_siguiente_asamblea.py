c85ec6f49fc8d9948a38f7af0a171a766bb7cc5c
lacinetika <lacinetika@sindominio.net>
2024-01-26 13:08:25 +0100

    Bugfix

--- calcular_siguiente_asamblea.py
+++ calcular_siguiente_asamblea.py
@@ -3,17 +3,10 @@ from datetime import datetime, timedelta, date
 from calendar import monthrange
 from typing import Callable
 
-# Sample JSON data
-json_data = '''
-{
-    "monday": { "18:00": "thai", "20:30": "box" },
-    "tuesday": { "19:30": "thai" },
-    "wednesday": { "18:00": "thai", "20:30": "box" },
-    "thursday": { "19:30": "thai" },
-    "friday": { "17:30": "libre" }
-}
-'''
+from settings import START_DATE, SCHEDULES
 
+start_date = datetime.strptime(START_DATE, '%Y-%m-%d')
+json_data = SCHEDULES
 day_names = ["monday", "tuesday", "wednesday", "thursday", "friday"]
 
 
@@ -31,13 +24,14 @@ class Activity:
 
 
 def parse_schedule(json_data):
-    schedule = json.loads(json_data)
-    for day in schedule:
-        schedule[day] = sorted(schedule[day].items())
-    return schedule
+    json_schedule = json.loads(json_data)
+    for day in json_schedule:
+        json_schedule[day] = sorted(json_schedule[day].items())
+    return json_schedule
 
 
 schedule = parse_schedule(json_data)
+
 
 def _find_next_activity_of_day(times, last_time):
     # Find the next time slot
@@ -54,13 +48,17 @@ def _find_next_activity_of_day(times, last_time):
         return next_activity_time, times[next_activity_time]
 
 
-def _calculate_next_date(start_date: datetime, end_date: datetime, cb: Callable[[Activity], None] = None):
+def _calculate_next_date(end_date: datetime, cb: Callable[[Activity], None] = None):
     # Current date for iteration, starting with the start date
     current_date = start_date
-    current_activity: Activity = None
     last_activity = {day: None for day in schedule.keys()}  # Track the last activity for each day
 
-    while current_date <= end_date:
+    while True:
+        # Calculate the next activity date
+        if current_date.weekday() == 4:  # Friday
+            current_date += timedelta(days=10)
+        else:
+            current_date += timedelta(weeks=2, days=1)
         day = current_date.strftime('%A').lower()
         if last_activity[day] is not None and len(schedule[day]) > 1:
             # get the times of the schedule from the dict object
@@ -69,35 +67,32 @@ def _calculate_next_date(start_date: datetime, end_date: datetime, cb: Callable[
             current_activity = Activity(next_schedule, current_date)
         else:
             current_activity = Activity(schedule[day][0], current_date)
-        last_activity[day] = current_activity
         if cb is not None:
             cb(current_activity)
-        # Calculate the next activity date
-        if current_date.weekday() == 4:  # Friday
-            current_date += timedelta(days=10)
-        else:
-            current_date += timedelta(weeks=2, days=1)
+        last_activity[day] = current_activity
+        if current_date > end_date:
+            break
     return current_activity
 
 
-def next_activity_from_date(start_date: datetime, today_date: datetime):
+def next_activity_from_date(today_date: datetime):
     """
     Calculate the next activity by a given day.
-    :param start_date:
     :param today_date:
     :return:
     """
-    activity = _calculate_next_date(start_date, today_date)
+    activity = _calculate_next_date(today_date)
     return activity
 
 
-def get_activities_between_dates(start_date: datetime, end_date: datetime):
+def get_activities_between_dates(end_date: datetime):
     # List to store all activity dates
     activities = []
-    _calculate_next_date(start_date, end_date, lambda a: activities.append(a))
+    _calculate_next_date(end_date, lambda a: activities.append(a))
     return activities
 
-def get_activites_of_a_month(start_date: datetime, year: int, month: int):
+
+def get_activites_of_a_month(year: int, month: int):
     # Convert start and end dates to datetime objects
     last_day = monthrange(year, month)[1]
     end_date = datetime(year, month, last_day)
@@ -106,22 +101,19 @@ def get_activites_of_a_month(start_date: datetime, year: int, month: int):
     def save_date(activity: Activity):
         if activity.date.year == year and activity.date.month == month:
             activities.append(activity)
-    _calculate_next_date(start_date, end_date, save_date)
+    _calculate_next_date(end_date, save_date)
     return activities
 
 
 def test():
     # Example usage
-    start_date = "2024-01-23"  # Replace with your start date
-    end_date = "2024-02-27"
-    # Convert start and today dates to datetime objects
-    start_date = datetime.strptime(start_date, '%Y-%m-%d')
+    end_date = "2024-01-27"
     end_date = datetime.strptime(end_date, '%Y-%m-%d')
     # Calculate next activity
-    activity_dates = next_activity_from_date(start_date, end_date)
+    activity_dates = next_activity_from_date(end_date)
     print("Next activity", activity_dates)
     # Calculate activity dates
-    activity_dates = get_activities_between_dates(start_date, datetime(2024, 10, 22))
+    activity_dates = get_activities_between_dates(datetime(2024, 10, 22))
     print("From now to endate", )
     for activity in activity_dates:
        print(activity)
@@ -130,7 +122,7 @@ def test():
     month = 2
     print("For month", year, month)
     # Calculate activity dates for the specified month
-    activity_dates_month = get_activites_of_a_month(start_date, year, month)
+    activity_dates_month = get_activites_of_a_month(year, month)
     for activity in activity_dates_month:
         print(activity)
 
---
+++ settings.py
@@ -0,0 +1,11 @@
+
+START_DATE = "2023-11-14"
+SCHEDULES = '''
+{
+    "monday": { "18:00": "thai", "20:30": "box" },
+    "tuesday": { "19:30": "thai" },
+    "wednesday": { "18:00": "thai", "20:30": "box" },
+    "thursday": { "19:30": "thai" },
+    "friday": { "17:30": "libre" }
+}
+'''
